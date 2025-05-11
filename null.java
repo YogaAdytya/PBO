8a60d70436096e5446cc3dafe8e99dc6da03dd48
YogaAdytya <Yogaadytya1605@gmail.com>
2025-05-10 23:58:53 -0700

    Update null.java

--- null.java
+++ null.java
@@ -15,15 +15,15 @@ class Main {
         Lingkaran l1 = new Lingkaran(5);
         Lingkaran l2 = l1;
         Lingkaran l3 = new Lingkaran(7);
-        System.out.println(l1.getJari2()); // Output: 5
-        System.out.println(l2.getJari2()); // Output: 5
-        System.out.println(l3.getJari2()); // Output: 7
+        System.out.println(l1.getJari2()); 
+        System.out.println(l2.getJari2()); 
+        System.out.println(l3.getJari2()); 
         l2 = null;
-        System.out.println(l1.getJari2()); // Output: 5
+        System.out.println(l1.getJari2()); 
         if (l2 != null) 
         {
-            System.out.println(l2.getJari2()); // Tidak akan dieksekusi
+            System.out.println(l2.getJari2());
         }
-        System.out.println(l3.getJari2()); // Output: 7
+        System.out.println(l3.getJari2());
     }
 }
